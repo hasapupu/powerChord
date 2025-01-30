cd26c930a26f2bfc488e6b3ee704ae4b11715216
cornL <dev.team.cornl@gmail.com>
2025-01-30 21:07:22 +0100

    main screen enviroment complete

--- nodes/enemySpawner.gd
+++ nodes/enemySpawner.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
-@onready var basicEnemy: PackedScene = preload("res://nodes/rangedEnemy.tscn")
+@onready var basicEnemy: PackedScene = preload("res://nodes/footSoldierEnemy.tscn")
+@onready var kysRangedEnemy: PackedScene = preload("res://nodes/rangedEnemy.tscn")
 @onready var playerGO = get_parent().get_node("player") 
 @onready var bruhTimer: Timer = get_node("Timer")
 var timeUntilSpawn = 3 
+var spawnLocs := [Vector2(450,20),Vector2(-450,20)]
 
 func instantiateBasicEnemy():
-	var currentBE: footSoldierEnemy = basicEnemy.instantiate()
+	var tempGoon = [basicEnemy,kysRangedEnemy].pick_random()
+	var currentBE: footSoldierEnemy = tempGoon.instantiate() as footSoldierEnemy
 	currentBE.playerPos = playerGO as player 
-	currentBE.position = Vector2(randi_range(-409,410),0)
+	currentBE.position = spawnLocs.pick_random()
 	add_child(currentBE)
 
 func _on_timer_timeout():
--- nodes/rangedEnemy.gd
+++ nodes/rangedEnemy.gd
@@ -1,19 +1,22 @@
 extends footSoldierEnemy
 
 @onready var susB: PackedScene = preload("res://nodes/bullett.tscn")
+@onready var animacion: AnimationPlayer = get_node("AnimationPlayer")
 
 func shoot():
-    shouldMove = false
-    await get_tree().create_timer(0.25).timeout
-    var tempB: bullet = susB.instantiate() as bullet
-    tempB.global_position = get_node("gun").global_position
-    tempB.target = playerPos.position * 1000
-    get_parent().add_child(tempB)
-    await get_tree().create_timer(0.25).timeout
-    shouldMove = true
+	animacion.play("stopMove")
+	shouldMove = false
+	await get_tree().create_timer(0.25).timeout
+	var tempB: bullet = susB.instantiate() as bullet
+	tempB.global_position = get_node("Sprite2D5/Sprite2D6/gun").global_position
+	tempB.target = playerPos.position * 1000
+	get_parent().add_child(tempB)
+	await get_tree().create_timer(0.25).timeout
+	shouldMove = true
+	animacion.play("startMove")
 
 func _on_timer_timeout():
-    shoot()
+	shoot()
 
 func _ready():
-    shoot()
+	shoot()
